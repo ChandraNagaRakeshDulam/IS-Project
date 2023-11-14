add19a864cd116750a0c7286e0bcdcd30f270a2f
ChandraNagaRakeshDulam <123144521+ChandraNagaRakeshDulam@users.noreply.github.com>
2023-11-13 20:11:37 -0500

    Add files via upload
    Game details uploaded

--- App.java
+++ App.java
@@ -1,3 +1,21 @@
+/*
+ Group 20
+ Team Members:
+ Rakesh Dulam,
+ Harshitha Sakamuri,
+ Varsha Thondalapally,
+ Nikhitha Sai Boyidapu
+
+ Project : 3D Tic Tac Toe
+ 
+ 3D Tic-Tac-Toe, also known as Qubic or 4x4x4 Tic-Tac-Toe, is a three-dimensional variation of the classic Tic-Tac-Toe game. 
+ The game is played on a 4x4x4 cubic grid, creating a total of 64 individual cells. 
+ The objective is similar to traditional Tic-Tac-Toe: players strive to align four of their markers in a row in any 
+ direction—horizontally, vertically, or diagonally.
+ */
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
